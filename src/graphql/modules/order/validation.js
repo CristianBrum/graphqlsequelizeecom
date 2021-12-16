6e573469e2fd1723c78f5a4cf1a55df4045066ee
cristian <cristian.brum2@yahoo.com.br>
2021-12-16 15:25:27 -0300

    fix: validacao de status

--- src/graphql/modules/order/validation.js
+++ src/graphql/modules/order/validation.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 
 const schema = Joi.object({
   installments: Joi.number().positive().max(12).required(),
-  status: Joi.number().min(0).max(3).required(),
+  status: Joi.number().min(0).max(2).required(),
   userId: Joi.number().required(),
   addressId: Joi.number().required(),
 });
