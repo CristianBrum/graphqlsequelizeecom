c5bb6196037edd1b6d09f2897121af7767a0c0d3
cristian <cristian.brum2@yahoo.com.br>
2021-12-16 09:53:59 -0300

    cria validacoes do schema de pedidos

--- src/graphql/modules/order/resolvers.js
+++ src/graphql/modules/order/resolvers.js
@@ -1,17 +1,39 @@
+const { UserInputError } = require('apollo-server');
 const { orders } = require('../../../models');
+const schema = require('./validation');
 
 const resolvers = {
   Query: {
-    order: (_, { id }) => {
+    order: (_, { id }, { auth }) => {
+      if (!auth) throw new Error('Você não tem autorização para essa ação!');
       return orders.findByPk(id);
     },
-    allOrders: () => {
+    allOrders: (_, args, { auth }) => {
+      if (!auth) throw new Error('Você não tem autorização para essa ação!');
       return orders.findAll();
     },
   },
   Mutation: {
-    async createOrder(_, { data }) {
+    async createOrder(_, { data }, { auth }) {
+      if (!auth) throw new Error('Você não tem autorização para essa ação!');
+      const { _value, error } = schema.validate(data, { abortEarly: false });
+      if (error) {
+        throw new UserInputError('Preencha todos os campos corretamente', {
+          validationErrors: error.details,
+        });
+      }
       return orders.create(data);
+    },
+    updateOrder: async (_, { id, data }, { auth }) => {
+      if (!auth) throw new Error('Você não tem autorização para essa ação!');
+      const findId = await orders.findByPk(id);
+      const updateOrder = await findId.update(data, { where: { id } });
+      return updateOrder;
+    },
+    deleteOrder: async (_, { id }, { auth }) => {
+      if (!auth) throw new Error('Você não tem autorização para essa ação!');
+      const deleteOrder = await orders.destroy({ where: { id } });
+      return deleteOrder;
     },
   },
   Order: {
---
+++ src/graphql/modules/order/validation.js
@@ -0,0 +1,11 @@
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+  installments: Joi.number().positive().max(12).required(),
+  total: Joi.number().positive().required(),
+  status: Joi.number().min(0).max(3).required(),
+  userId: Joi.number().required(),
+  addressId: Joi.number().required(),
+});
+
+module.exports = schema;
